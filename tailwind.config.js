6eb505a50eb07b2e71b0131cd53349ca3dc824a5
Grinti123 <mirzaagilramadhan@gmail.com>
2024-11-23 00:16:23 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: ["business"],
+  },
+  theme: {
+    colors: {
+      'orangegruvbox' : '#fabd2f',
+      'darkgruvbox' : '#1d2021'
+    },
+    extend: {
+      fontFamily: {
+        jetbrains: ["JetBrains Mono", "serif"],
+      }
+    },
+  },
+  plugins: [require('daisyui'), 
+    require('tailwind-typewriter') ({
+      wordsets: {
+        hero: {
+          words: ['Creative', 'Discipline', 'Consistent'],
+          caretColor: '#fabd2f'
+        }
+      }
+    })
+  ],
+}
+
